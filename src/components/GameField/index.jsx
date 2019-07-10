2994f7b7c19b6c8326ee05141ba279424227215a
Darina Kuleva <darinakuleva@gmail.com>
2019-07-10 19:25:41 +0300

    ISSUE-4 Рефакторинг заполнения поля

--- src/components/GameField/index.jsx
+++ src/components/GameField/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import field from './style.module.css'
+import { connect } from 'react-redux'
 
 class Field extends React.PureComponent {
+
+  state = {
+  GameNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null]
+}
+
   render() {
 
-    const GameNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null]
+    const {
+      GameList
+    } = this.props
+
+    const {
+      GameNumbers
+    } = this.state
 
     const compareRandom = (a, b) => {
       return Math.random() - 0.5
@@ -12,19 +24,42 @@ class Field extends React.PureComponent {
 
     GameNumbers.sort(compareRandom)
 
+    const sizeWidthField = 4;
+    const sizeHeightField = 4;
+    let amountGameNumbers = 0;
+
+    for (let width=1;  width<=sizeWidthField; width++) {
+      for (let height=1;  height<=sizeHeightField; height++) {
+        let fieldCell = {
+          value:GameNumbers[amountGameNumbers],
+          line: width,
+          column: height
+        }
+        GameList.push(fieldCell)
+        amountGameNumbers++;
+      }
+    }
+
     return (
-      <section className={field.wrapper}>
-        {GameNumbers.map((number) => (
-          (
-            number === null ?
-              <button key={16}>null</button> :
-              <button key={number}>{number}</button>
-          )
-        ))}
-      </section>
+      <>
+        <section className={field.wrapper}>
+          {GameList.map((number) => (
+            (
+              number.value === null ?
+                <button key={16}>null</button> :
+                <button key={number.value}>{number.value}</button>
+            )
+          ))}
+        </section>
+      </>
     )
   }
-
 }
 
-export default Field
+function mapStateToProps(state) {
+  return {
+    GameList: state.GameList,
+  }
+}
+
+export default connect(mapStateToProps)(Field)
